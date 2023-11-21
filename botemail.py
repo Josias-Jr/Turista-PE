fbe9d4a1c7cccc2a96c7679a70e28489e6f511e5
Vitor Cavalcanti <124193127+VitorCavalcantigo@users.noreply.github.com>
2023-11-21 12:03:50 -0300

    Add files via upload

---
+++ botemail.py
@@ -0,0 +1,57 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import mysql.connector
+
+conexao = mysql.connector.connect(
+    host='localhost',
+    database='turismo',
+    user='root',
+    password='Francisca27032002'
+)
+
+cursor = conexao.cursor()
+
+cursor.execute("SELECT email FROM cliente")
+
+emails = cursor.fetchall()
+
+cursor.close()
+conexao.close()
+
+remetente = 'turistape23@gmail.com'
+senha = 'drueznpymuigzuqv'
+
+for email in emails:
+    destinatario = email[0]
+
+    mensagem = MIMEMultipart()
+    mensagem['From'] = remetente
+    mensagem['To'] = destinatario
+    mensagem['Subject'] = 'Descubra a Costa Nautica da coroa do Avião com RotaPE - Lugares imperdíveis!'
+
+    corpo = f'''
+    <html>
+        <body>
+            <p>Olá,</p>
+            <p>Você já imaginou explorar os destinos mais incríveis do mundo com facilidade e economia? Com o Turismundo, tornamos seus sonhos de viagem realidade...</p>
+            <p><img src="URL_DA_IMAGEM" alt="Descrição da Imagem"></p>
+            <p>Para mais informações, visite nosso <a href="URL_DO_SITE">site</a>.</p>
+            <p>Atenciosamente,</p>
+            <p>Equipe Turismundo</p>
+        </body>
+    </html>
+    '''
+
+    mensagem.attach(MIMEText(corpo, 'html'))
+
+    try:
+        servidor_smtp = smtplib.SMTP('smtp.gmail.com', 587)
+        servidor_smtp.starttls()
+        servidor_smtp.login(remetente, senha)
+        texto_email = mensagem.as_string()
+        servidor_smtp.sendmail(remetente, destinatario, texto_email)
+        servidor_smtp.quit()
+        print('E-mail enviado para', destinatario)
+    except Exception as e:
+        print('Ocorreu um erro ao enviar o e-mail para', destinatario, str(e))
