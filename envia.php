47a2656c7df04c0489b2184ed9b0e85683d467b3
Vitor Cavalcanti <vitorgomes1942@gmail.com>
2023-11-21 17:34:45 -0300

    Update envia.php

--- envia.php
+++ envia.php
@@ -17,7 +17,7 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-$sql = "INSERT INTO sua_tabela (nome, email, senha) VALUES ('$name', '$email', '$password')";
+$sql = "INSERT INTO cliente (nome, email, senha) VALUES ('$name', '$email', '$password')";
 
 if ($conn->query($sql) === TRUE) {
     echo "Mensagem enviada com sucesso!";
