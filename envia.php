213bed940fbdfccc354eb14fe8416ea04e1f2158
Vitor Cavalcanti <vitorgomes1942@gmail.com>
2023-11-27 22:16:35 -0300

    senha

--- envia.php
+++ envia.php
@@ -2,7 +2,7 @@
 
 $servername = "localhost";
 $username = "root";
-$password = "Francisca27032002";
+$password = "";
 $dbname = "turismo";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
